ccc8a14177a3cdbc9e17f3aad5c73fe96f776adf
Jessica <jessil46@uci.edu>
2024-04-19 21:23:13 -0700

    Changed welcome page

--- src/components/welcome.jsx
+++ src/components/welcome.jsx
@@ -5,8 +5,8 @@ import headshot from "../images/welcome/website_headshot.jpg";
 import UCILogo from "../images/welcome/UCILogo.png";
 import UtahIcon from "../images/welcome/Utah.jpg";
 import LAIcon from "../images/welcome/LA.png";
-import PythonIcon from "../images/welcome/C++Logo.png";
-import CIcon from "../images/welcome/PythonLogo.png";
+import PythonIcon from "../images/welcome/PythonLogo.png";
+import CIcon from "../images/welcome/C++Logo.png";
 import JavaScriptIcon from "../images/welcome/JavaScriptLogo.png";
 import HTMLIcon from "../images/welcome/HTMLLogo.jpg";
 import ReactIcon from "../images/welcome/ReactLogo.png";
@@ -26,119 +26,104 @@ function Welcome() {
             </div>
           </div>
 
-          <div className="container">
-            <div className="half-block">
-              <br />
-              <div>
-                <div className="subtitle">Hello world!</div>
+          <div className="vertical_container">
+            <div className="container">
+              <div className="half-block">
+                <br />
+                <div>
+                  <div className="subtitle">Hello world!</div>
+                  <p>
+                    My name is Jessica Lin, and welcome to my website. I coded
+                    it from scratch using Javascript, React, HTML, and CSS.
+                  </p>
+                </div>
+                <br />
+                <br />
+                <br />
+                <div className="subtitle">About me:</div>
+                <div className="logo_container">
+                  <img
+                    src={UCILogo}
+                    alt="University of California, Irvine"
+                    className="circular-image"
+                  />
+                  University of California, Irvine
+                </div>
+                <div className="logo_container">
+                  <img
+                    src={UtahIcon}
+                    alt="Salt Lake City, Utah"
+                    className="circular-image"
+                  />
+                  Salt Lake City, Utah
+                </div>
+                <div className="logo_container">
+                  <img
+                    src={LAIcon}
+                    alt="Los Angeles"
+                    className="circular-image"
+                  />
+                  Los Angeles Metrapolitan Area
+                </div>
+              </div>
+              <div className="half-block">
                 <p>
-                  My name is Jessica Lin, and welcome to my website. I coded it
-                  from scratch using Javascript, React, HTML, and CSS.
+                  <br />I have classroom and personal experience working with
                 </p>
-              </div>
-              <br />
-              <br />
-              <br />
-              <div className="subtitle">About me:</div>
-              <div className="logo_container">
-                <img
-                  src={UCILogo}
-                  alt="University of California, Irvine"
-                  className="circular-image"
-                />
-                University of California, Irvine
-              </div>
-              <div className="logo_container">
-                <img
-                  src={UtahIcon}
-                  alt="Salt Lake City, Utah"
-                  className="circular-image"
-                />
-                Salt Lake City, Utah
-              </div>
-              <div className="logo_container">
-                <img
-                  src={LAIcon}
-                  alt="Los Angeles"
-                  className="circular-image"
-                />
-                Los Angeles Metrapolitan Area
+                <br />
+                <p>
+                  <div className="logo_container">
+                    <img
+                      src={PythonIcon}
+                      alt="Python"
+                      className="circular-image"
+                    />
+                    Python
+                  </div>
+                  <div className="logo_container">
+                    <img src={CIcon} alt="C++" className="circular-image" />
+                    C++
+                  </div>
+                  <div className="logo_container">
+                    <img
+                      src={JavaScriptIcon}
+                      alt="JavaScript"
+                      className="circular-image"
+                    />
+                    JavaScript
+                  </div>
+                  <div className="logo_container">
+                    <img src={HTMLIcon} alt="HTML" className="circular-image" />
+                    HTML
+                  </div>
+                  <div className="logo_container">
+                    <img
+                      src={ReactIcon}
+                      alt="React"
+                      className="circular-image"
+                    />
+                    React
+                  </div>
+                  <div className="logo_container">
+                    <img src={CSSIcon} alt="CSS" className="circular-image" />
+                    CSS
+                  </div>
+                </p>
+                <br />
               </div>
             </div>
-            <div className="half-block">
+            <div className="container">
               <p>
-                <br />I have classroom and personal experience working with
-              </p>
-              <br />
-              <p>
-                <div className="logo_container">
-                  <img
-                    src={PythonIcon}
-                    alt="Los Angeles"
-                    className="circular-image"
-                  />
-                  Python
-                </div>
-                <div className="logo_container">
-                  <img
-                    src={CIcon}
-                    alt="Los Angeles"
-                    className="circular-image"
-                  />
-                  C++
-                </div>
-                <div className="logo_container">
-                  <img
-                    src={JavaScriptIcon}
-                    alt="Los Angeles"
-                    className="circular-image"
-                  />
-                  JavaScript
-                </div>
-                <div className="logo_container">
-                  <img
-                    src={HTMLIcon}
-                    alt="Los Angeles"
-                    className="circular-image"
-                  />
-                  HTML
-                </div>
-                <div className="logo_container">
-                  <img
-                    src={ReactIcon}
-                    alt="Los Angeles"
-                    className="circular-image"
-                  />
-                  React
-                </div>
-                <div className="logo_container">
-                  <img
-                    src={CSSIcon}
-                    alt="Los Angeles"
-                    className="circular-image"
-                  />
-                  CSS
-                </div>
-                {/* <br />• Python <br />• C++ <br />• JavaScript
-                <br />• HTML
-                <br />• CSS
-                <br /> */}
-              </p>
-              <br />
-              {/* <p>
                 In my freetime, I enjoy thrifting, gaming, working out, playing
-                poker, and finding new music!
-              </p>
-              <br />
-              <p>
-                Feel free to reach out to me anytime at
+                poker, and finding new music! Feel free to reach out to me
+                anytime at{" "}
                 <a href="mailto:linjjessica@gmail.com" className="email-link">
                   linjjessica@gmail.com
                 </a>
                 .
-              </p> */}
+              </p>
+              <br />
             </div>
-            <div className="container"></div>
           </div>
         </div>
       </div>
